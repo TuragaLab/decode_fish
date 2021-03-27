fix psf scaling

decode_fish/engine/microscope.py
@@ -44,9 +44,9 @@ class Microscope(nn.Module):
 
     def __init__(self,
                  parametric_psf: List[torch.nn.Module]=None,
-                 empirical_psf : List[torch.nn.Module] = None ,
-                 noise: Union[torch.nn.Module,  None]=None,
-                 scale: float = 10000., multipl = 100
+                 empirical_psf : List[torch.nn.Module]=None ,
+                 noise: Union[torch.nn.Module, None]=None,
+                 scale: float = 10000., multipl=100
                  ):
 
         super().__init__()
@@ -74,14 +74,14 @@ class Microscope(nn.Module):
                 for emper_psf in self.empirical_psf:
                     psf += emper_psf(x_os_val, y_os_val, z_os_val)
             #normalizing psf
+            torch.clamp_min_(psf,0)
             psf = psf.div(psf.sum(dim=[2, 3, 4], keepdim=True))
             #applying intenseties (N_Emitters, C, H, W, D)
 
-            #try * self.scale
             psf = psf * i_val[:,None,None,None,None]
             xsim = place_psf(locations, psf, output_shape)
             xsim = self.scale * xsim * self.multipl
-            torch.clamp_min_(xsim,0)
+#             torch.clamp_min_(xsim,0)
             #torch.clamp_max_(xsim,255)
             if eval_:
                 return xsim, psf

decode_fish/engine/point_process.py
@@ -17,27 +17,28 @@ class PointProcessUniform(Distribution):
         bg(bool): if returns sampled backround
 
     """
-    def __init__(self, local_rate: torch.tensor, min_int: float):
+    def __init__(self, local_rate: torch.tensor, min_int: float, sim_iters: int = 5):
 
         self.local_rate = local_rate
         self.device = self._get_device(self.local_rate)
         self.min_int = torch.tensor(min_int, device=self.device)
+        self.sim_iters = sim_iters
 
-    def sample(self, N:int =1):
+    def sample(self):
 
-        if N == 1: return self._sample()
-        else:
-            res_ = [self._sample() for i in range(N)]
-            locations = torch.cat([i[0] for i in res_], dim=1)
-            x_offset = torch.cat([i[1] for i in res_], dim=1)
-            y_offset = torch.cat([i[2] for i in res_], dim=1)
-            z_offset = torch.cat([i[3] for i in res_], dim=1)
-            intensities = torch.cat([i[4] for i in res_], dim=1)
-            return locations, x_offset, y_offset, z_offset, intensities
+        res_ = [self._sample(self.local_rate/self.sim_iters) for i in range(self.sim_iters)]
+        locations = torch.cat([i[0] for i in res_], dim=0)
+        x_offset = torch.cat([i[1] for i in res_], dim=0)
+        y_offset = torch.cat([i[2] for i in res_], dim=0)
+        z_offset = torch.cat([i[3] for i in res_], dim=0)
+        intensities = torch.cat([i[4] for i in res_], dim=0)
 
-    def _sample(self):
+        return tuple(locations.T), x_offset, y_offset, z_offset, intensities, res_[0][5]
 
-        locations = D.Bernoulli(self.local_rate).sample()
+    def _sample(self, local_rate):
+
+        local_rate = torch.clamp_max(local_rate, 1.)
+        locations = D.Bernoulli(local_rate).sample()
         n_emitter = int(locations.sum().item())
         zero_point_five = torch.tensor(0.5, device=self.device)
         x_offset = D.Uniform(low=0 - zero_point_five, high=0 + zero_point_five).sample(sample_shape=[n_emitter])
@@ -46,7 +47,7 @@ class PointProcessUniform(Distribution):
         intensities = D.Uniform(low=self.min_int, high=1.0).sample(sample_shape=[n_emitter])
 
         output_shape = tuple(locations.shape)
-        locations = locations.nonzero(as_tuple=True)
+        locations = locations.nonzero(as_tuple=False)
         return locations, x_offset, y_offset, z_offset, intensities, output_shape
 
 

decode_fish/funcs/evaluation.py
@@ -4,7 +4,7 @@ __all__ = ['matching']
 
 # Cell
 from ..imports import *
-from .output_trafo import model_output_to_df
+from .output_trafo import SIPostProcess
 from scipy.spatial.distance import cdist
 from  decode_fish.funcs.emitter_io import *
 

decode_fish/funcs/plotting.py
@@ -22,9 +22,10 @@ def add_colorbar(im, aspect=20, pad_fraction=0.5, **kwargs):
     return im.axes.figure.colorbar(im, cax=cax, **kwargs)
 
 # Cell
-def sl_plot(x, xsim, pred_df, background, res):
+def sl_plot(x, xsim, pred_df, target_df, background, res):
 
     pred_df = pred_df[pred_df['frame_idx']==0]
+    target_df = target_df[target_df['frame_idx']==0]
     with torch.no_grad():
         fig = plt.figure(figsize=(20,4))
         plt.subplot(151)
@@ -35,7 +36,8 @@ def sl_plot(x, xsim, pred_df, background, res):
 
         plt.subplot(152)
         im = plt.imshow(xsim[0][0].cpu().numpy().max(0))
-        plt.scatter(pred_df['x']/100, pred_df['y']/100,facecolors='red', edgecolors='red', marker='+', s=20)
+        plt.scatter(target_df['x']/100, target_df['y']/100,facecolors='black', edgecolors='black', marker='x', s=25.)
+        plt.scatter(pred_df['x']/100, pred_df['y']/100,facecolors='red', edgecolors='red', marker='o', s=5.)
         add_colorbar(im)
         plt.axis('off')
         plt.title('Sim. image')
@@ -85,7 +87,7 @@ def gt_plot(x, pred_df, gt_df, px_size, gt_rec=None):
             im = plt.imshow(gt_rec.max(0), vmax=vmax)
             add_colorbar(im)
             plt.axis('off')
-            plt.title('RMSE ' + str(np.round(np.sqrt(((x-gt_rec)**2).max()),2)))
+            plt.title('RMSE ' + str(np.round(np.sqrt(((x-gt_rec)**2).mean()),2)))
 
             plt.subplot(133)
             im = plt.imshow(max_proj - gt_rec.max(0))

decode_fish/funcs/utils.py
@@ -52,6 +52,6 @@ def plot_tb_logs(exps, metric='Sim. Metrics/eff_3d', window_len=1):
         all_vals.append(vals)
 
     for v,e in zip(all_vals,exps):
-        plt.plot(smooth(v, window_len), label=e)
-        print(np.array(v).max().round(2), np.array(v).min().round(2), len(v), e)
+        plt.plot(smooth(v, window_len), label=e.split('/')[-1])
+#         print(np.array(v).max().round(2), np.array(v).min().round(2), len(v), e)
     plt.legend()

decode_fish/funcs/visualization.py
@@ -40,8 +40,11 @@ def get_simulation_statistics(decode_dl, micro, int_threshold=1, samples = 1):
 
             x = x[0,0,z_ind].cpu().numpy()
 
-            xsim = micro(*PointProcessUniform(local_rate, min_int=decode_dl.min_int).sample())
+            sim_vars = PointProcessUniform(local_rate, min_int=decode_dl.min_int).sample()
+            xsim = micro(*sim_vars)
             xsim_noise = micro.noise(xsim, background).sample()
+            sim_df = sample_to_df(*sim_vars[:-1])
+            sim_df = sim_df[sim_df['frame_idx'] == 0]
 
             xsim_noise = xsim_noise.cpu().numpy()[0,0,z_ind]
             background = background[0,0].cpu().numpy()
@@ -61,34 +64,35 @@ def get_simulation_statistics(decode_dl, micro, int_threshold=1, samples = 1):
             im = plt.imshow(xsim_noise)
             add_colorbar(im)
             plt.title('Sim. Image ')
+            plt.scatter(sim_df['x']/100,sim_df['y']/100, color='red', s=5.)
 
             plt.show()
 
 # Cell
-def get_prediction(model, img, micro=None, cuda=True, px_size=np.array([100.,100.,100.]), return_rec=False, threshold=0.1):
+def get_prediction(model, post_proc, img, micro=None, cuda=True, return_rec=False):
 
     with torch.no_grad():
 
         img = img[(None,)*(5-img.ndim)]
         model.eval().cuda() if cuda else model.eval().cpu()
         res_dict = model(img.cuda()) if cuda else model(img)
-        pred_df = model_output_to_df(res_dict, threshold, px_size=px_size)
+        pred_df = post_proc(res_dict)
 
         if return_rec:
-            ae_img_3d = micro(*model_output_to_micro_input(res_dict, threshold=threshold))
-            return pred_df, ae_img_3d + res_dict['background']
+            ae_img_3d = micro(*post_proc(res_dict, ret='micro'))
+            return pred_df, ae_img_3d + res_dict['background'], res_dict
 
         return pred_df
 
-def eval_random_crop(decode_dl, model, micro, projection='mean', px_size=np.array([100.,100.,100.]), cuda=False, threshold=0.1, samples=1):
+def eval_random_crop(decode_dl, model, post_proc, micro, projection='mean', cuda=False, samples=1):
 
     with torch.no_grad():
 
         for _ in range(samples):
 
             x, local_rate, background = next(iter(decode_dl))
-            pred_df, rec = get_prediction(model, x[:1], micro=micro, cuda=True, return_rec=True, threshold=threshold)
-            pred_df = nm_to_px(pred_df, px_size)
+            pred_df, rec, res_dict = get_prediction(model, post_proc, x[:1], micro=micro, cuda=True, return_rec=True)
+            pred_df = nm_to_px(pred_df, post_proc.px_size)
 
             x = x[0,0].cpu().numpy()
             rec = rec[0,0].cpu().numpy()
@@ -104,7 +108,9 @@ def eval_random_crop(decode_dl, model, micro, projection='mean', px_size=np.arra
 
             axes[1].set_title('Reconstruction', size=16)
 
-def eval_random_sim(decode_dl, model, micro, projection='mean', px_size=np.array([100.,100.,100.]), cuda=False, threshold=0.1, samples=1):
+            pred_df, rec,res_dict
+
+def eval_random_sim(decode_dl, model, post_proc, micro, projection='mean', plot_gt=True, cuda=True, samples=1):
 
     with torch.no_grad():
 
@@ -115,14 +121,21 @@ def eval_random_sim(decode_dl, model, micro, projection='mean', px_size=np.array
             sim_vars = PointProcessUniform(local_rate, min_int=decode_dl.min_int).sample()
             xsim = micro(*sim_vars)
             x = micro.noise(xsim, background).sample()
-
-            pred_df, rec = get_prediction(model, x[:1], micro=micro, cuda=True, return_rec=True, threshold=threshold)
-            pred_df = nm_to_px(pred_df, px_size)
+            pred_df, rec, res_dict = get_prediction(model, post_proc, x[:1], micro=micro, cuda=True, return_rec=True)
+            pred_df = nm_to_px(pred_df, post_proc.px_size)
+            sim_df = nm_to_px(sample_to_df(*sim_vars[:-1]))
+            sim_df = sim_df[sim_df['frame_idx']==0]
 
             x = x[0,0].cpu().numpy()
             rec = rec[0,0].cpu().numpy()
 
             axes = plot_3d_projections(x, projection=projection)
+
+            if plot_gt:
+                axes[0].scatter(sim_df['x'],sim_df['y'], color='black', marker='x', s=25.)
+                axes[1].scatter(sim_df['x'],sim_df['z'], color='black', marker='x', s=25.)
+                axes[2].scatter(sim_df['y'],sim_df['z'], color='black', marker='x', s=25.)
+
             axes[0].scatter(pred_df['x'],pred_df['y'], color='red', s=5.)
             axes[1].scatter(pred_df['x'],pred_df['z'], color='red', s=5.)
             axes[2].scatter(pred_df['y'],pred_df['z'], color='red', s=5.)
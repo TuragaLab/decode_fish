add fractal noise, fix microscope max scaling

decode_fish/engine/microscope.py
@@ -39,25 +39,29 @@ class Microscope(nn.Module):
     """
 
 
-    def __init__(self, psf: torch.nn.Module=None, noise: Union[torch.nn.Module, None]=None, scale: float = 10000.):
+    def __init__(self, psf: torch.nn.Module=None, noise: Union[torch.nn.Module, None]=None, scale: float = 10000., norm='max', sum_fac=1):
 
         super().__init__()
         self.psf = psf
         self.scale = scale
         self.noise = noise
+        self.norm = norm
 
         self.theta = self.noise.theta
+        self.sum_fac = sum_fac
 
     def forward(self, locations, x_os_val, y_os_val, z_os_val, i_val, output_shape, bg=None, eval_=None):
 
         if len(locations[0]):
 
+            if 'max' in self.norm:
+                psf_norm = self.psf.psf_volume.max()
+            else:
+                psf_norm = torch.clamp_min(self.psf.psf_volume, 0).sum()/self.sum_fac
             # Apply continuous shift
             psf = self.psf(x_os_val, y_os_val, z_os_val)
             torch.clamp_min_(psf,0)
-            # normalize psf
-            psf_max = psf.amax(dim=[2, 3, 4], keepdim=True)
-            psf = psf.div(psf_max)
+            psf = psf/psf_norm
             # applying intenseties
             tot_intensity = torch.clamp_min(i_val, 0)
             psf = psf * tot_intensity[:,None,None,None,None]

decode_fish/funcs/file_io.py
@@ -170,8 +170,10 @@ def load_psf_noise_micro(cfg):
 
     psf = load_psf(cfg)
     noise = hydra.utils.instantiate(cfg.noise)
-    micro = hydra.utils.instantiate(cfg.microscope, psf=psf, noise=noise).cuda()
-
+    if 'max' in cfg.microscope.norm:
+        micro = hydra.utils.instantiate(cfg.microscope, psf=psf, noise=noise).cuda()
+    else:
+        micro = hydra.utils.instantiate(cfg.microscope, psf=psf, noise=noise, sum_fac=psf.psf_volume.sum().item()).cuda()
     return psf, noise, micro
 
 def load_post_proc(cfg):
@@ -182,8 +184,10 @@ def load_post_proc(cfg):
 
 def get_dataloader(cfg):
 
-    imgs_3d        = [load_tiff_image(f)[0] for f in sorted(glob.glob(cfg.data_path.image_path))]
-    estimate_backg = hydra.utils.instantiate(cfg.bg_estimation)
+    imgs_3d       = [load_tiff_image(f)[0] for f in sorted(glob.glob(cfg.data_path.image_path))]
+    gen_bg        = [hydra.utils.instantiate(cfg.bg_estimation.smoothing)]
+    if cfg.bg_estimation.fractal.scale:
+        gen_bg.append(hydra.utils.instantiate(cfg.bg_estimation.fractal))
     roi_masks     = [get_roi_mask(img, tuple(cfg.roi_mask.pool_size), percentile= cfg.roi_mask.percentile) for img in imgs_3d]
 
     min_shape = tuple(np.stack([v.shape for v in imgs_3d]).min(0)[-3:])
@@ -197,7 +201,7 @@ def get_dataloader(cfg):
 #     probmap_generator = ScaleTensor(low=cfg.prob_generator.low,
 #                                     high=cfg.prob_generator.high)
 #     rand_scale = RandScale(0.1,1.)
-#     rate_tfms = [estimate_backg, probmap_generator, rand_scale]
+#     rate_tfms = [gen_bg, probmap_generator, rand_scale]
 
     probmap_generator = UniformValue(cfg.prob_generator.low, cfg.prob_generator.high)
     rate_tfms = [probmap_generator]
@@ -211,7 +215,7 @@ def get_dataloader(cfg):
     ds = DecodeDataset(path = cfg.data_path.image_path,
                        dataset_tfms =  dataset_tfms,
                        rate_tfms = rate_tfms,
-                       bg_transform = estimate_backg,
+                       bg_tfms = gen_bg,
                        device='cuda:0',
                        num_iter=(cfg.training.num_iters) * cfg.training.bs)
 

decode_fish/funcs/visualization.py
@@ -111,7 +111,7 @@ def eval_random_crop(decode_dl, model, post_proc, micro, proj_func=np.max, cuda=
 
             combine_figures([fig1,fig2], ['Predictions', 'Residual'], figsize=(20,10))
 
-def eval_random_sim(decode_dl, model, post_proc, micro, projection='mean', plot_gt=True, cuda=True, samples=1):
+def eval_random_sim(decode_dl, model, post_proc, micro, proj_func=np.max, plot_gt=True, cuda=True, samples=1):
 
     with torch.no_grad():
 
@@ -130,15 +130,9 @@ def eval_random_sim(decode_dl, model, post_proc, micro, projection='mean', plot_
             x = x[0,0].cpu().numpy()
             rec = rec[0,0].cpu().numpy()
 
-            axes = plot_3d_projections(x, projection=projection)
-            plot_dfs = [pred_df]
-            if plot_gt:
-                plot_dfs.append(sim_df)
+            fig1, axes = plot_3d_projections(x, proj_func=proj_func, display=False)
+            scat_3d_projections(axes, [pred_df])
 
-            scat_3d_projections(axes, plot_dfs)
+            fig2, axes = plot_3d_projections(x, proj_func=proj_func, display=False)
 
-            axes[1].set_title('Predictions', size=16)
-
-            axes = plot_3d_projections(rec, projection=projection)
-
-            axes[1].set_title(f'Reconstruction', size=16)
+            combine_figures([fig1,fig2], ['Predictions', 'Reconstruction'], figsize=(20,10))